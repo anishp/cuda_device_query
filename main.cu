363836f3cedcece5bf3ba9f9d1b17f2a2beb1ac7
Anish Pednekar <pednekar.anish@gmail.com>
2014-02-06 05:52:37 +0530

    basic query

--- main.cu
+++ main.cu
@@ -17,7 +17,12 @@ int main(int argc, char** argv[])
 		err = cudaGetDeviceProperties(&mydevice, 0);
 		if(err != cudaSuccess) printf("%sn", cudaGetErrorString(err));
 		
-		printf("\tCuda device %d: %s\n", i, mydevice.name);
+		printf("  Cuda device %d: %s\n", i, mydevice.name);
+		printf("  Multiprocessors: %d\n", mydevice.multiProcessorCount);
+		printf("  Device Clock: %d KHz\n", mydevice.clockRate);
+		printf("  Memory Clock Rate (KHz): %d\n", mydevice.memoryClockRate);
+		printf("  Peak Memory Bandwidth (GB/s): %f\n",
+           2.0*mydevice.memoryClockRate*(mydevice.memoryBusWidth/8)/1.0e6);
 	}
 
 	getchar();
